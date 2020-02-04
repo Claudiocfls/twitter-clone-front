c6de4b30561dbebc79ebeab78b15b71f3fe9e65b
claudiocfls <claudiosilva.cfls@gmail.com>
2020-02-04 08:57:34 -0300

    feat: add like event to socket listener

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,18 +4,33 @@ class Socket {
   constructor() {
     this.io = socket('http://localhost:3001');
     this.tweetCallbacks = [];
-    this.handleCallbacks = this.handleCallbacks.bind(this);
-    this.io.on('tweet', this.handleCallbacks);
+    this.likeCallbacks = [];
+
+    this.handleTweetCallbacks = this.handleTweetCallbacks.bind(this);
+    this.io.on('tweet', this.handleTweetCallbacks);
+
+    this.handleLikeCallbacks = this.handleLikeCallbacks.bind(this);
+    this.io.on('like', this.handleLikeCallbacks);
   }
 
-  handleCallbacks(tweet) {
+  handleTweetCallbacks(tweet) {
     this.tweetCallbacks.forEach(cb => {
+      cb(tweet);
+    });
+  }
+
+  handleLikeCallbacks(tweet) {
+    this.likeCallbacks.forEach(cb => {
       cb(tweet);
     });
   }
 
   subscribeToNewTweets(cb) {
     this.tweetCallbacks.push(cb);
+  }
+
+  subscribeToNewLikes(cb) {
+    this.likeCallbacks.push(cb);
   }
 }
 
