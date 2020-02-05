397b52481fef21b99837d17a099321ed89534dbc
claudiocfls <claudiosilva.cfls@gmail.com>
2020-02-04 23:37:08 -0300

    chore: finish the responsiveness of layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,41 +6,26 @@ import SidePanelRight from '../SidePanelRight/SidePanelRight';
 
 const useStyles = makeStyles(theme => ({
   container: {
-    // padding: '0 300px',
-    // height: '',
     position: 'relative',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
   },
   leftPanel: {
-    // height: '80vh',
     width: '270px',
     position: '-webkit-sticky',
     position: 'sticky',
     alignSelf: 'flex-start',
     top: 0,
-    // [theme.breakpoints.down('md')]: {
-    //   height: '80vh',
-    //   width: '70px',
-    //   position: 'fixed',
-    //   left: '50px',
-    // },
+    [theme.breakpoints.down('md')]: {
+      width: '70px',
+    },
   },
   rightPanel: {
     alignSelf: 'flex-start',
-    // height: '80vh',
     position: 'sticky',
     top: 0,
     width: '380px',
-    // position: 'fixed',
-    // right: '300px',
-    [theme.breakpoints.down('md')]: {
-      height: '80vh',
-      width: '380px',
-      position: 'fixed',
-      right: '50px',
-    },
     [theme.breakpoints.down('sm')]: {
       display: 'none',
     },
@@ -54,48 +39,21 @@ const useStyles = makeStyles(theme => ({
     border: '1px solid rgb(230, 236, 240)',
     backgroundColor: '#FFF',
     height: '54px',
-    // width: 'calc(100vw - 600px - 270px - 380px - 2px)',
     width: 'calc(100% - 2px)',
     position: 'sticky',
     top: 0,
-    // position: 'fixed',
-    // left: '570px',
-    // top: 0,
-    // position: 'relative',
-    // top: 0,
     left: '-50px',
     zIndex: 2,
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    // padding: '0 12px',
     '& h1': {
       fontSize: '18px',
       marginLeft: '12px',
     },
-    [theme.breakpoints.down('md')]: {
-      width: 'calc(100vw - 100px - 70px - 380px - 2px)',
-      left: '120px',
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: 'calc(100vw - 100px - 70px - 2px)',
-      left: '120px',
-    },
   },
   pageContent: {
-    // paddingTop: '55px',
-    // position: 'absolute',
-    // left: '570px',
-    // width: 'calc(100vw - 600px - 270px - 380px)',
     width: '100%',
-    [theme.breakpoints.down('md')]: {
-      width: 'calc(100vw - 100px - 70px - 380px)',
-      left: '120px',
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: 'calc(100vw - 100px - 70px)',
-      left: '120px',
-    },
   },
 }));
 
