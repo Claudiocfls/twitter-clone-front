37589333844b03b4f710fd6b81314f3ccc4dd289
claudiocfls <claudiosilva.cfls@gmail.com>
2020-02-08 13:49:49 -0300

    chore: use loading on home page and improve token handling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,15 +3,31 @@ import TweetList from '../../components/TweetList';
 import Layout from '../../components/Layout';
 import TweetTextArea from '../../components/TweetTextArea/TweetTextArea';
 import api from './../../services/api';
+import Loading from '../../components/Loading/Loading';
 import { useAuth0 } from "../../react-auth0-spa";
+
+const fetchTokenOrGetLocal = async (fetchToken) => {
+  const tokenLocal = sessionStorage.getItem('twitter-clone-token');
+  
+  if (tokenLocal) {
+    return Promise.resolve(tokenLocal);
+  }
+
+  const token = await fetchToken();
+  
+  sessionStorage.setItem('twitter-clone-token', token);
+  
+  return Promise.resolve(token);
+};
 
 const Home = () => {
   const [tweets, setTweets] = useState([]);
-  const use = useAuth0();
+  const { loading, user, getTokenSilently } = useAuth0();
+  const [userEmail, setUserEmail] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const token = await use.getTokenSilently();
+      const token = await fetchTokenOrGetLocal(getTokenSilently);
       const { data } = await api.get('/tweets', {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -23,11 +39,21 @@ const Home = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if(user) {
+      const { email } = user;
+      setUserEmail(email);
+    }
+  }, [loading]);
+
+  const showLoading = loading || tweets.length < 1;
+
   return (
     <Layout>
       <TweetTextArea />
       <hr style={{border: '8px solid #e6ecf0', margin: 0,}} />
-      <TweetList tweets={tweets} />
+      {showLoading && <Loading />}
+      {!showLoading && <TweetList tweets={tweets} />}
     </Layout>
   );
 }
